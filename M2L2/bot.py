6e1db994ccae4973bea12db9adba136d0e8e006e
AleksTitan <144429423+AleksTiGit@users.noreply.github.com>
2025-03-01 10:52:29 +0300

    Дз комплитэ

--- M2L2/bot.py
+++ M2L2/bot.py
@@ -1,6 +1,7 @@
 import telebot, os
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
+from config import *
 
 load_dotenv("./.env")
 
@@ -15,6 +16,13 @@ def keyboard_func():
   keyboard.add(InlineKeyboardButton("meat", callback_data="meat"))
   return keyboard
 
+def keyboard_func_city():
+  keyboard = InlineKeyboardMarkup()
+  keyboard.add(InlineKeyboardButton("Люберцы", callback_data="Lubertsi"))
+  keyboard.add(InlineKeyboardButton("Домодедово", callback_data="Domodedovo"))
+  keyboard.add(InlineKeyboardButton("Видное", callback_data="Vidnoe"))
+  return keyboard 
+
 @bot.message_handler(commands=['start', 'help'])
 def start_func(message):
   bot.reply_to(message, "Привет, я Эко-бот")
@@ -22,6 +30,10 @@ def start_func(message):
 @bot.message_handler(commands=["eco"])
 def eco_func(message):
   bot.send_message(message.chat.id, "Для какого предмета хочешь узнать срок разложения", reply_markup=keyboard_func())
+
+@bot.message_handler(commands=["city"])
+def city_func(message):
+  bot.send_message(message.chat.id, "Выбери город, в котором хочешь узнать пункты переработки", reply_markup=keyboard_func_city())
 
 @bot.callback_query_handler(func=lambda call: call.data in ["paper", "glass", "plastic", "foil", "meat"])
 def buttons_func(call):
@@ -32,5 +44,26 @@ def buttons_func(call):
               "meat": "от 1 месяца"}
   bot.answer_callback_query(call.id)
   bot.send_message(call.message.chat.id, f"Время разложения: {eco_list[call.data]}")
+
+@bot.callback_query_handler(func=lambda call: call.data in ["Lubertsi", "Domodedovo", "Vidnoe"])
+def buttons_func(call):
+  city_list = {"Lubertsi": Lubertsi,
+              "Domodedovo": Domodedovo,
+              "Vidnoe": Vidnoe}
+  
+  selected_city = city_list[call.data]
+  bot.answer_callback_query(call.id)
+  bot.send_message(call.message.chat.id, 
+      f"Бумага: {selected_city['paper']}\n"
+      f"Стекло: {selected_city['glass']}\n"
+      f"Пластик: {selected_city['plastic']}\n"
+      f"Металл: {selected_city['metal']}\n"
+      f"Одежда: {selected_city['cloth']}\n"
+      f"Лампочки: {selected_city['bulds']}\n"
+      f"Крышки: {selected_city['caps']}\n"
+      f"Техника: {selected_city['technique']}\n"
+      f"Батарейки: {selected_city['batteries']}\n"
+      f"Шины: {selected_city['tires']}\n"
+      f"Опасное: {selected_city['danger']}")
 
 bot.infinity_polling()
